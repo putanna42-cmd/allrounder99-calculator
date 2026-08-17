7e9b830fa72ee321498b64d503e2ea1ea96b79e4
putanna42-cmd <putanna42@gmail.com>
2026-08-17 15:22:51 +0530

    Add complete calculator app and Android APK build

---
+++ app/src/main/assets/sw.js
@@ -0,0 +1,1 @@
+const C='a99-calculator-v1';const A=['./','index.html','style.css','app.js','manifest.webmanifest','icons/icon-192.png','icons/icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
---
+++ app/src/main/java/com/allrounder99/calculator/MainActivity.java
@@ -0,0 +1,27 @@
+package com.allrounder99.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+    @Override public void onCreate(Bundle state) {
+        super.onCreate(state);
+        webView = new WebView(this);
+        setContentView(webView);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setAllowFileAccess(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+    @Override public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack(); else super.onBackPressed();
+    }
+}
